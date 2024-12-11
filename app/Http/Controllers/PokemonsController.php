c4a9470b09e46676dd5f8dbcb358983306052854
MrZero <yanemmanuel00@gmail.com>
2024-12-11 19:28:10 -0300

    feat: Search Pokemon

--- app/Http/Controllers/PokemonsController.php
+++ app/Http/Controllers/PokemonsController.php
@@ -7,8 +7,20 @@ use Illuminate\Http\Request;
 
 class PokemonsController extends Controller
 {
-    public function pokemons(){
-        $pokemon = Pokemon::with('status')->paginate(10);
+    public function pokemons(Request $request)
+    {
+        $name = $request->query('name');
+        if(!isset($name))
+        {
+            $name = '';
+        }
+        $searchable = Pokemon::with('status')->where('name', 'like', '%'.$name.'%')->paginate(20);
+        return $searchable;
+    }
+
+    public function pokemons_id($id)
+    {
+        $pokemon = Pokemon::with('status')->where('id', $id)->firstOrFail();
         return $pokemon;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ use App\Http\Controllers\PokemonsController;
 use App\Http\Controllers\TesteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PokemonsController::class, 'pokemons']);
+Route::prefix('/')->controller(PokemonsController::class)->group(function(){
+    Route::get('/', 'pokemons');
+    Route::get('pokemon/{id}', 'pokemons_id');
+    Route::post('create', 'create_pokemon');
+});
